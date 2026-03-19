31bc80958d303eca0ca049603720cc4e24a6e744
unix-follower <61003331+unix-follower@users.noreply.github.com>
2026-03-19 20:35:40 +0300

    Add GitHub Actions pipeline (#1)
    Co-authored-by: Artsem Nikitsenka <artsem_nikitsenka@epam.com>

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,7 +2,7 @@ import { registerOTel, OTLPHttpJsonTraceExporter } from "@vercel/otel"
 
 export function register() {
   registerOTel({
-    serviceName: "assistant-on-nextjs",
+    serviceName: "science-ui-on-nextjs",
     traceExporter: new OTLPHttpJsonTraceExporter({
       url: "http://192.168.105.8:4318/v1/traces",
     }),
